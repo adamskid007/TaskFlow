b96432ad2a114e60d085368c36c8b498850d4c6b
adamskid <adamusman.bincom@gmail.com>
2026-06-22 11:19:04 +0100

    connect frontend to render backend updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://taskflow-api-orqj.onrender.com",
+    baseURL: "https://taskflow-api-orqj.onrender.com/api",
 });
 
 export default api;
